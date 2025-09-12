e64c8286e0a6b15305c287d074461b1499473db0
dubl0ck <dubl0ck@hotmail.com>
2025-09-12 14:57:36 +0200

    reset

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 // app.js
+import { createClient } from 'https://esm.sh/@supabase/supabase-js'
+
+const supabaseUrl = "https://qehhrhsmlrsxqugvuida.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp4aXB5d3lob2J0bHhhYWVyYXppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTczMzE5ODYsImV4cCI6MjA3MjkwNzk4Nn0.YB_mgNKRBrJ8-Z7jnT5_xeQV0zrmAiRqVZ8JqgLxjVs";
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 // Referencias DOM
 const form = document.getElementById("marcaForm");
