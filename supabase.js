55b4317be16ab3524ef7537719a6357959d081d9
dubl0ck <dubl0ck@hotmail.com>
2025-09-12 14:09:15 +0200

    errores conexión

--- supabase.js
+++ supabase.js
@@ -1,5 +1,6 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js'
+// supabase.js
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://zxipywyhobtlxaaerazi.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp4aXB5d3lob2J0bHhhYWVyYXppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTczMzE5ODYsImV4cCI6MjA3MjkwNzk4Nn0.YB_mgNKRBrJ8-Z7jnT5_xeQV0zrmAiRqVZ8JqgLxjVs";
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabaseUrl = "https://qehhrhsmlrsxqugvuida.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFlaGhyaHNtbHJzeHF1Z3Z1aWRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc2NzcyMjQsImV4cCI6MjA3MzI1MzIyNH0.HA9VzmLuCUVe35iKNJqrBGZ6rWXcu_q7GPrNJAMUytQ";
+export const supabase = createClient(supabaseUrl, supabaseKey);
